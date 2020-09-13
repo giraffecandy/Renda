bd1937a6e3041b155386e0acca9398d09377db32
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-13 16:32:52 +0900

    finish

--- app/src/main/java/app/babachan/renda/MainActivity.kt
+++ app/src/main/java/app/babachan/renda/MainActivity.kt
@@ -1,9 +1,9 @@
 package app.babachan.renda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,20 +14,36 @@ class MainActivity : AppCompatActivity() {
 
         var tapCountTextView: TextView = findViewById(R.id.tapCount)
         var tapCountText: Int = 0
-        var countTextView:Int = 10
-        val timer: CountDownTimer = object :CountDownTimer(10000, 1000){}
+        var countTime: Int = 10
+        val timer: CountDownTimer = object : CountDownTimer(10000, 1000) {
+            override fun onFinish() {
+                startButton.isVisible = true
+                tapButton.setBackgroundResource(R.drawable.tap_button)
+                countTime = 10
+                tapCountText = 0
+            }
+
+            override fun onTick(p0: Long) {
+
+                tapButton.setBackgroundResource(R.drawable.tap_button)
+                countTime = countTime - 1
+                countTimeNumber.text = countTime.toString()
+            }
+        }
 
         tapButton.setOnClickListener {
-            tapCountText += 1
-            tapCountTextView.text = tapCountText.toString()
+            if (countTime < 10) {
+                tapCountText += 1
+                tapCountTextView.text = tapCountText.toString()
+            }
         }
 
         startButton.setOnClickListener {
 
-            tapCountText == 0
             tapCountTextView.text = tapCountText.toString()
             startButton.isVisible = false
 
+            timer.start()
 
         }
     }
